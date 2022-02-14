ece53e17521d62903d6af4b913c053a44e9cca28
Romuald Brillout <git@brillout.com>
2022-02-14 09:49:26 +0100

    wip

--- api/telefunc.js
+++ api/telefunc.js
@@ -1,7 +1,11 @@
-import { telefunc } from "telefunc";
+import { telefunc, telefuncConfig } from "telefunc";
 import "../dist/server/importBuild.js";
 
+telefuncConfig.debug = true
+
 export default async (req, res) => {
+  console.log('request :'+req.url)
+  console.log('request body :'+req.body)
   const httpResponse = await telefunc({
     url: req.url,
     body: req.body,
